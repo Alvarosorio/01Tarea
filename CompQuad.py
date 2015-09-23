75a865a5d4d923feb8bcb487869dd6a6b4209fd9
Alvaro <alvarosorio11@gmail.com>
2015-09-23 16:17:16 -0300

    Ahora si mide el tiempo de ejecucion

--- CompQuad.py
+++ CompQuad.py
@@ -2,6 +2,7 @@ from scipy.integrate import *
 from split import *
 import numpy as np
 from planck import *
+import time
 
 #En este script se compara el algoritmo implementado para calcular la funcion
 #de planck con la funcion de scipy integrate.quad
@@ -13,7 +14,7 @@ from planck import *
 #ya que la funcion se indefine en este valor
 ini = 0.000001
 fin = np.pi/2
-
+start_1 = time.time()
 #discretizamos el domino a integrar
 delta = np.linspace(ini, fin, num=1000)
 m = delta.shape
@@ -26,9 +27,13 @@ for k in range(0, m[0]-2):
 #la funcion planck es la integral a resolver luego de aplicar el cambio de variables
     simp = (paso/3.0) * (planck(delta[k]) + 4*(planck(delta[k+1])) + planck(delta[k+2]))
     S += simp
+time_1 = time.time() - start_1
 
+start_2 = time.time()
 I, err = quad(planck, 0, np.pi/2)
+time_2 = time.time() - start_2
 
-print "Metodo Implementado =", S
-print "integrate.quad =", I
+
+print "Metodo Implementado =", S, "Tiempo =", time_1 
+print "integrate.quad =", I, "Tiempo =", time_2 
 print "diferencia =", I-S
--- CompTrapz.py
+++ CompTrapz.py
@@ -1,6 +1,7 @@
 from scipy.integrate import *
 from split import *
 import numpy as np
+import time
 
 #En este script se compara el algoritmo implementado para calcular el Espectro
 #del sol con la funcion de scipy integrate.trapz
@@ -16,14 +17,17 @@ D = split(T)
 n = D.shape
 
 #integracion ordinaria, mas ordinaria que el vino en bolsa
-
+start_1 = time.time()
 I = 0
 for j in range(0, n[1]-1):
     gap = D[0, j+1] - D[0, j]
     trap = ((D[1, j] + D[1, j+1]) * gap/2)
     I += trap
+time_1 = time.time() - start_1
 
+start_2 = time.time()
 T = trapz(D[1, :], x=D[0, :])
+time_2 = time.time() - start_2
 
-print "Metodo Implementado =", I
-print "integrate.trapz =", T
+print "Metodo Implementado =", I, "Tiempo =", time_1 
+print "integrate.trapz =", T, "Tiempo =", time_2 
